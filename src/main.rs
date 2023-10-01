f7efdee0b62f1154b67df2603d3be18fb43fab5e
xStFtx <you@example.com>
2023-10-01 08:00:33 +0200

    1.1

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
 use std::{
     env,
     error::Error,
+    fs::File,
+    io::{BufRead, BufReader},
 };
 
-cosnt SHA1_HEDX_STRING_LENGTH: usize = 40;
+const SHA1_HEX_STRING_LENGTH: usize = 40;
 
 fn main() -> Result<(), Box<dyn Error>> {
     let args: Vec<String> = env::args().collect();
@@ -11,8 +13,23 @@ fn main() -> Result<(), Box<dyn Error>> {
     if args.len() != 3 {
         println!("Usage:");
         println!("sha1: <wordlist.txt> <sha1_hash>");
-        return;
+        return Ok(());
     }
+
+    let hash_to_crack = args[2].trim();
+    if hash_to_crack.len() != SHA1_HEX_STRING_LENGTH {
+        return Err("sha1 hash is not valid".into());
+    }
+
+    let wordlist_file = File::open(&args[1])?;
+    let reader = BufReader::new(&wordlist_file);
+
+    for line in reader.lines() {
+        let line = line?.trim().to_string();
+        println!("{}", line);
+    }
+
+    Ok(())
     
-    
+
 }
